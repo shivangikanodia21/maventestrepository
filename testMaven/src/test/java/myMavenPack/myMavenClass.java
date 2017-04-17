bcbf40cddcff361732cdef406dcfd98033506e65
hp <hp@hp-PC.lan>
2017-04-17 19:39:57 +0530

    initial commit

---
+++ testMaven/src/test/java/myMavenPack/myMavenClass.java
@@ -0,0 +1,21 @@
+package myMavenPack;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class myMavenClass {
+	
+	@Test
+	public void printdata()
+	{
+		System.out.println("hello dear");
+		//Assert.assertEquals('a', 'b');
+	}
+	
+	@Test
+	public void printdata1()
+	{
+		System.out.println("how are you?");
+	}
+
+}
